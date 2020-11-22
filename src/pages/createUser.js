ddc93b7357a8cf4a3c4733ba2426950e99f4436a
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2020-11-22 18:52:25 -0300

    Remove rotas

--- src/pages/createUser.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const CreateUser = props => {
-  return (
-    <pre>create user</pre>
-  )
-}
-
-export default CreateUser;
--- src/pages/login.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Login = props => {
-  return (
-    <pre>login</pre>
-  )
-}
-
-export default Login;
