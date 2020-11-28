bff028f9804e978171e37afe7694649979e38d8b
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2020-11-27 23:36:35 -0300

    Adiciona redux e id na url para books

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+// import {connect} from 'react-redux';
 import {find} from 'lodash';
 import Input from './Input';
 import Button from './Button';
@@ -14,21 +15,34 @@ const Login = props => {
     const user = find(content, item => item.email === data.email);
     const passwordIsRight = user && user.password === data.password;
     if (!user || !passwordIsRight) setWarning(true);
-    else window.location.assign('/books');
+    else window.location.assign(`/books/${user.id}`);
   }
 
-  const fetchAPI = async () => {
+  const getUsers = async () => {
     const payload = await get('users');
     authenticate(payload);
   }
+
+  // console.log('REDUX', props.globalState.user);
 
   return (
     <Panel>
       <Input isMobile={true} warning={warning} label="Email" onChange={param => setData({email: param, password: data.password})} />
       <Input isMobile={true} warning={warning} type='password' label="Senha" onChange={param => setData({email: data.email, password: param})}/>
-      <Button variant='gradient' size={50} onClick={() => fetchAPI()}>Entrar</Button>
+      <Button variant='gradient' size={50} onClick={getUsers}>Entrar</Button>
     </Panel>
   )
 }
 
+// const dispatcher = dispatch => {
+//   return {
+//     setUser: payload => dispatch({type: 'SET_USER', payload}),
+//   }
+// }
+
+// const getState = state => {
+//   return {globalState: state};
+// }
+
+// export default connect(getState, dispatcher)(Login);
 export default Login;
